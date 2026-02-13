77783c7fb414d5087b3d9d2506859d45e3ab45c4
Nicolas Frisby <nicolas.frisby@moduscreate.com>
2026-02-13 12:36:15 -0500

    add MkRoundTrip to Parser.Tests

--- test/Language/Matter/Parser/Tests.hs
+++ test/Language/Matter/Parser/Tests.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Language.Matter.Parser.Tests (tests) where
 
 import Control.Monad (foldM, unless)
 import Data.Functor ((<&>))
 import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
 import Data.Text.Lazy qualified as TL
 import Data.Text.Lazy.IO qualified as TL
 import Data.Word (Word32)
 import Language.Matter.Parser qualified as P
 import Language.Matter.Tokenizer
+import Language.Matter.Tokenizer.Counting (Four (..))
 import Language.Matter.Tokenizer.Pretty (prettyToken, toLazyText)
 import Language.Matter.Tokenizer.Tests (printWithPositions)
-import Language.Matter.SyntaxTree qualified as ST
+import Language.Matter.SyntaxTree
 import Language.Matter.SyntaxTree.Generator (generateMatter, shrinkMatter)
 import Language.Matter.SyntaxTree.Pretty (pretty)
 import System.Exit (exitFailure)
@@ -34,7 +37,7 @@ tests = do
             print sz
             g <- getStdGen
             -- TODO how to inject some unnecessary OdWhitespace tokens? Should not be adjacent to OdWhiteSpace.
-            TL.putStrLn $ runStateGen_ g $ \g' -> toLazyText $ foldMap (prettyToken g') $ pretty (x :: ST.Matter ST.P NonEmpty [])
+            TL.putStrLn $ runStateGen_ g $ \g' -> toLazyText $ foldMap (prettyToken g') $ pretty (x :: Matter P NonEmpty [])
             putStrLn ""
     QC.sample' (QC.sized $ \sz -> (,) sz <$> generateMatter) >>= mapM_ f
 
@@ -121,7 +124,10 @@ testCases = [
 
   ]
 
-data TestCase = MkTestCase String Bool
+data TestCase =
+    MkTestCase String Bool
+  |
+    MkRoundTrip Int (Matter P NonEmpty [])
 
 failing :: String -> TestCase
 failing s = MkTestCase s False
@@ -130,22 +136,29 @@ passing :: String -> TestCase
 passing s = MkTestCase s True
 
 doesItPass :: TestCase -> IO Bool
-doesItPass (MkTestCase s expected) = do
-    let x = parseWhole s
-    let (itPasses, prefix, extra) = case x of
-            ParseDone{} -> (expected, if expected then "PASS " else "FAIL ", "")
-            ParseStuck _ _ _ stk -> (not expected, if expected then "FAIL " else "PASS ", show (P.simplify stk))
-            TokenizerError{} -> (False, "BAD ", "")
-    unless itPasses $ do
-        printWithPositions s
-        putStr prefix
-        print x
-        putStrLn extra
-    pure itPasses
+doesItPass = \case
+    MkTestCase s expected -> do
+        let x = parseWhole s
+        let (itPasses, prefix, extra) = case x of
+                ParseDone{} -> (expected, if expected then "PASS " else "FAIL ", "")
+                ParseStuck _ _ _ stk -> (not expected, if expected then "FAIL " else "PASS ", show (P.simplify stk))
+                TokenizerError{} -> (False, "BAD ", "")
+        unless itPasses $ do
+            printWithPositions s
+            putStr prefix
+            print x
+            putStrLn extra
+        pure itPasses
+    MkRoundTrip g m ->
+        QC.quickCheckWithResult QC.stdArgs{QC.maxSuccess = 1} (QC.once $ prop_prettyThenParseIsSame' (g, m)) <&> \case
+            QC.Success{} -> True
+            QC.GaveUp{} -> False
+            QC.Failure{} -> False
+            QC.NoExpectedFailure{} -> False
 
 -----
 
-type M = ST.Matter Pos NonEmpty []
+type M = Matter Pos NonEmpty []
 
 data ParseResult =
     ParseDone M
@@ -177,35 +190,36 @@ parseWhole x =
 -- equivalence at type 'Matter'.
 prop_prettyThenParseIsSame :: QC.Property
 prop_prettyThenParseIsSame =
-    QC.propertyForAllShrinkShow gen shrnk noshow prop
+    QC.propertyForAllShrinkShow gen shrnk noshow prop_prettyThenParseIsSame'
   where
     gen = liftA2 (,) (QC.choose (minBound, maxBound)) generateMatter
     shrnk (g, m) = [ (g, m') | m' <- shrinkMatter m]
     noshow _ = []
 
-    forgetPos = ST.fold $ ST.embed . ST.mapPositions (\_ -> ST.MkP)
+prop_prettyThenParseIsSame' :: (Int, Matter P NonEmpty []) -> QC.Property
+prop_prettyThenParseIsSame' (g, m) =
+    QC.counterexample ("m = " <> show m)
+  $ QC.counterexample ("g = " <> show g)
+  $ QC.counterexample ("txt = " <> TL.unpack txt)
+  $ case parseWhole txt of
+        ParseDone m' ->
+            QC.counterexample ("m' = " <> show m')
+          $ QC.counterexample "ParseDone"
+          $ m == forgetPos m'
+        ParseStuck l mbTk r stk ->
+            QC.counterexample ("stk = " <> show stk)
+          $ QC.counterexample ("(l, mbTk, r) = " <> show (l, mbTk, r))
+          $ QC.counterexample "ParseStuck"
+          $ QC.property False
+        TokenizerError l r e ->
+            QC.counterexample ("(l, e, r) = " <> show (l, e, r))
+          $ QC.counterexample "TokenizerError"
+          $ QC.property False
+  where
+    forgetPos = fold $ embed . mapPositions (\_ -> MkP)
 
-    prop (g, m) =
-        let txt =
-                runStateGen_ (mkStdGen g)
-              $ \g' -> toLazyText
-              $ foldMap (prettyToken g')
-              $ pretty (m :: ST.Matter ST.P NonEmpty [])
-        in
-        QC.counterexample ("m = " <> show m)
-      $ QC.counterexample ("g = " <> show g)
-      $ QC.counterexample ("txt = " <> TL.unpack txt)
-      $ case parseWhole txt of
-            ParseDone m' ->
-                QC.counterexample ("m' = " <> show m')
-              $ QC.counterexample "ParseDone"
-              $ m == forgetPos m'
-            ParseStuck l mbTk r stk ->
-                QC.counterexample ("stk = " <> show stk)
-              $ QC.counterexample ("(l, mbTk, r) = " <> show (l, mbTk, r))
-              $ QC.counterexample "ParseStuck"
-              $ QC.property False
-            TokenizerError l r e ->
-                QC.counterexample ("(l, e, r) = " <> show (l, e, r))
-              $ QC.counterexample "TokenizerError"
-              $ QC.property False
+    txt =
+        runStateGen_ (mkStdGen g)
+      $ \g' -> toLazyText
+      $ foldMap (prettyToken g')
+      $ pretty (m :: Matter P NonEmpty [])
